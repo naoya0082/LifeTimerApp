23b92386cb27559eaad78928742f00adce740e13
naoya0082 <6ds3f7@gmail.com>
2018-11-08 14:01:08 +0900

    Initial Commit

---
+++ LifeTimerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifeTimerApp
+//
+//  Created by 高橋直也 on 2018/11/08.
+//  Copyright © 2018 Takahashi Naoya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
